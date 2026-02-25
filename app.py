dccaae539389015fe0e801411f226d89a0b6c16d
Sudeshna Das <sudeshnadas201516@gmail.com>
2026-02-25 14:38:02 +0530

    added gunicon for production deployment

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def chat():
     return str(response["answer"])
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
